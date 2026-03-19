5d6a8ab9eb36b4eabfe41fceb636ebe8e8187db9
Diego Miguel <diegomiguel.1806@gmail.com>
2026-03-19 10:42:50 +0100

    Rename variable 'g' to 'p95' for clarity

--- src/main/java/org/example/historico/RegistroHistorico.java
+++ src/main/java/org/example/historico/RegistroHistorico.java
@@ -4,12 +4,12 @@ public class RegistroHistorico {
 
     // Atributos, el id no es necesario porque se gestiona al generar el Json de historico
     public String fecha;  // Fecha diaria
-    public double g;  // Gasolina 95
+    public double p95;  // Gasolina 95
     public double d;  // Diesel
 
     public RegistroHistorico(String fecha, double p95, double d) {
         this.fecha = fecha;
-        this.g = p95;
+        this.p95 = p95;
         this.d = d;
     }
 }
