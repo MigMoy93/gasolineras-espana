3625834d75e88574efd4da0f0188bc94fb3e6153
Diego Miguel <diegomiguel.1806@gmail.com>
2026-03-19 10:21:11 +0100

    Add new fields to Gasolinera class and update constructor

--- src/main/java/org/example/model/Gasolinera.java
+++ src/main/java/org/example/model/Gasolinera.java
@@ -3,23 +3,53 @@ package org.example.model;
 public class Gasolinera {
 
     public String id;
-    public String r;
-    public String m;
+    public String rotulo;
+    public String direccion;
+    public String municipio;
+    public String provincia;
+    public String cp;
+    public String horario;
+    public String tipoVenta;
+
     public double lat;
     public double lon;
-    public double p95;
-    public double d;
 
-    public Gasolinera(String id, String rotulo, String municipio,
+    public double p95;
+    public double p98;
+    public double diesel;
+    public double dieselPremium;
+
+    public double glp;
+    public double gnc;
+    public double gnl;
+
+    public Gasolinera(String id, String rotulo, String direccion,
+                      String municipio, String provincia, String cp,
+                      String horario, String tipoVenta,
                       double lat, double lon,
-                      double p95, double diesel) {
+                      double p95, double p98,
+                      double diesel, double dieselPremium,
+                      double glp, double gnc, double gnl) {
 
         this.id = id;
-        this.r = rotulo;
-        this.m = municipio;
+        this.rotulo = rotulo;
+        this.direccion = direccion;
+        this.municipio = municipio;
+        this.provincia = provincia;
+        this.cp = cp;
+        this.horario = horario;
+        this.tipoVenta = tipoVenta;
+
         this.lat = lat;
         this.lon = lon;
+
         this.p95 = p95;
-        this.d = diesel;
+        this.p98 = p98;
+        this.diesel = diesel;
+        this.dieselPremium = dieselPremium;
+
+        this.glp = glp;
+        this.gnc = gnc;
+        this.gnl = gnl;
     }
 }
